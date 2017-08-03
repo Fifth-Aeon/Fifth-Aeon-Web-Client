e6500d2298f79c267d395aa2acb264fb3d98cc49
William Ritson <william.ritson@gmail.com>
2017-08-03 12:29:33 -0700

    Added new deployment urls

--- src/app/url.ts
+++ src/app/url.ts
@@ -1,6 +1,6 @@
 const isLocal = location.host.includes('localhost');
-const serverWsUrn = isLocal ? 'localhost:2222' : 'ws-battleship.herokuapp.com';
-const serverHttpUrn = isLocal ? 'localhost:4200' : 'battleship.williamritson.com'
+const serverWsUrn = isLocal ? 'localhost:2222' : 'ws-ccg.herokuapp.com';
+const serverHttpUrn = isLocal ? 'localhost:4200' : 'ccg-game.firebaseapp.com'
 const serverWs = isLocal ? 'ws' : 'wss';
 const serverHttp = isLocal ? 'http' : 'https';
 
