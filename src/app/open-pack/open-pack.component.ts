3239fbc943a7a2f0da91e197100f3cfd2e8b876a
William Ritson <william.ritson@gmail.com>
2018-02-18 22:53:34 -0800

    Improved look of store componant

--- src/app/open-pack/open-pack.component.ts
+++ src/app/open-pack/open-pack.component.ts
@@ -15,24 +15,33 @@ export class OpenPackComponent implements OnInit {
   collection: Collection;
   cards: Card[];
   working: boolean;
+  message: string;
 
   constructor(
     decks: DecksService,
     private collectionService: CollectionService
   ) {
     this.collection = collectionService.getCollection();
+    this.defaultMessage();
+  }
+
+  defaultMessage() {
+    this.message = `You have ${this.collection.getPackCount()} pack(s) and ${this.collection.getGold()} gold.`;
   }
 
   async open() {
     this.working = true;
+    this.message = 'Communicating with server.';
     this.cards = await this.collectionService.openPack();
+    this.defaultMessage();
     this.working = false;
-
   }
 
   async buy() {
     this.working = true;
+    this.message = 'Communicating with server.';
     await this.collectionService.buyPack();
+    this.defaultMessage();
     this.working = false;
   }
 
