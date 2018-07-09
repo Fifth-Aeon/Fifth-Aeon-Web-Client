ae11ec18de9a355234d9ebb45c4cf14300415cbe
William Ritson <william.ritson@gmail.com>
2018-07-08 21:39:59 -0700

    Added ability to generate standalone executables

---
+++ package-app.js
@@ -0,0 +1,18 @@
+const packager = require('electron-packager')
+const fs = require('fs');
+
+const copy = (from, to) => {
+    fs.createReadStream(from).pipe(fs.createWriteStream(to));
+}
+
+copy('electron-index.js', 'dist/index.js');
+copy('package.json', 'dist/package.json');
+
+packager({
+    executableName: 'Fifth Aeon',
+    dir: 'dist',
+    out: 'standalone',
+    all: 'true'
+}).then(appPaths => {
+    console.log('Done, created:', appPaths.join(', '));
+});
