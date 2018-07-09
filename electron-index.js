dbf63f43c1bf6d7b1aa9de714ae13381e6567915
William Ritson <william.ritson@gmail.com>
2018-07-09 16:38:33 -0700

    Small changes to electron config

--- electron-index.js
+++ electron-index.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({fullscreen: true})
+  mainWindow = new BrowserWindow({width: 800, height: 800})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
