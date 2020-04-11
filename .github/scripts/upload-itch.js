09ea2e2de1efd694585eddaf83d2ac5ce8844289
William Ritson <william.ritson@gmail.com>
2020-04-10 22:58:47 -0400

    Fix platform sources.
    log more debug info in upload.

--- .github/scripts/upload-itch.js
+++ .github/scripts/upload-itch.js
@@ -5,27 +5,33 @@ const game = "fifth-aeon"
 const sourceFolder = "standalone"
 const platformInfo = {
     linux: {
-        source: "fifth-aeon-win32-x64",
-        channel: "fifth-aeon-linux",
+        source: "fifth-aeon-linux-x64",
+        channel: "linux",
         butler: "linux-amd64"
     },
     win32: {
-        source: "fifth-aeon-linux-x64",
-        channel: "fifth-aeon-win64",
+        source: "fifth-aeon-win32-x64",
+        channel: "win64",
         butler: "windows-amd64"
     },
     darwin: {
         source: "fifth-aeon-darwin-x64",
-        channel: "fifth-aeon-mac",
+        channel: "mac",
         butler: "darwin-amd64"
     }
 }
 
 async function main() {
     const currentPlatformInfo = platformInfo[process.platform];
-    await utils.exec(`curl -L -o butler.zip https://broth.itch.ovh/butler/${currentPlatformInfo.butler}/LATEST/archive/default`)
-    await utils.exec(`unzip ./butler.zip`)
-    await utils.exec(`./butler push ${sourceFolder}/${currentPlatformInfo.source} ${account}/${game}:${currentPlatformInfo.channel}`)
+    console.log(process.platform, '->', currentPlatformInfo);
+    try {
+        await utils.exec(`curl -L -o butler.zip https://broth.itch.ovh/butler/${currentPlatformInfo.butler}/LATEST/archive/default`, { echo: true })
+        await utils.exec(`unzip ./butler.zip`, { echo: true })
+        await utils.exec(`./butler push ${sourceFolder}/${currentPlatformInfo.source} ${account}/${game}:${currentPlatformInfo.channel}`, { echo: true })
+    } catch (e) {
+        console.error(e.error);
+        process.exit(e.code);
+    }
 }
 
 main()
